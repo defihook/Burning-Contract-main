476dd5da2618e2c2177069c27dd857c3503ca82c
Franky soldev <frankykevin.dev@gmail.com>
2024-10-08 15:38:53 +0900

    Update deploy.ts

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -1,6 +1,6 @@
 // Migrations are an early feature. Currently, they're nothing more than this
 // single deploy script that's invoked from the CLI, injecting a provider
-// configured from the workspace's Anchor.toml.
+
 
 const anchor = require("@project-serum/anchor");
 
